0fc26a0bd18d2ed64937f87497c20cd4ac0a4832
ManasSarker <sarker01960@gmail.com>
2022-09-25 00:08:49 +0600

    fetch all data in shop section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./componenets/Header/Header";
+import Shop from "./componenets/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
